8b8c98a523d0ac74a44438af02c6500d45fa2c15
ellisvelandia <eyis619@gmail.com>
2022-10-11 05:50:58 -0500

    3d-car

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import "./style.css";
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import {
+  EffectComposer,
+  DepthOfField,
+  Bloom,
+  ChromaticAberration,
+} from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
+import {
+  OrbitControls,
+  PerspectiveCamera,
+  CubeCamera,
+  Environment,
+} from "@react-three/drei";
+
+import { Boxes } from "./Boxes";
 import { Ground } from "./Ground";
+import { FloatingGrid } from "./FloatingGrid";
+import { Car } from "./Car";
+import { Rings } from "./Rings";
 
 function CarShow() {
   return (
@@ -11,6 +28,15 @@ function CarShow() {
       <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
 
       <color args={[0, 0, 0]} attach="background" />
+
+      <CubeCamera resolution={256} frames={Infinity}>
+        {(texture) => (
+          <>
+            <Environment map={texture} />
+            <Car />
+          </>
+        )}
+      </CubeCamera>
 
       <spotLight
         color={[1, 0.25, 0.7]}
@@ -31,6 +57,26 @@ function CarShow() {
         shadow-bias={-0.0001}
       />
       <Ground />
+      <FloatingGrid />
+      <Boxes />
+      <Rings />
+
+      <EffectComposer>
+        <DepthOfField focusDistance={0.0035} focalLength={0.01} bokehScale={3} height={480} />
+        <Bloom
+          blendFunction={BlendFunction.ADD}
+          intensity={1.3} // The bloom intensity.
+          width={300} // render width
+          height={300} // render height
+          kernelSize={5} // blur kernel size
+          luminanceThreshold={0.15} // luminance threshold. Raise this value to mask out darker elements in the scene.
+          luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
+        />
+        <ChromaticAberration
+          blendFunction={BlendFunction.NORMAL} // blend mode
+          offset={[0.0005, 0.0012]} // color offset
+        />
+      </EffectComposer>
     </>
   );
 }
